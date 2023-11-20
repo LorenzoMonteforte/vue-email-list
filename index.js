ef05055b552564799554fc278faaf5d64edd396a
Lorenzo Monteforte <lorenzomonteforte.mm@mail.com>
2023-11-20 15:27:10 +0100

    Bonus

--- index.js
+++ index.js
@@ -2,16 +2,21 @@ const {createApp} = Vue
 createApp({
     data(){
         return{
-            email : []
+            email : [],
+            numeroEmail : 10,
+            finish : false
         }
     },
     methods : {
         scaricaEmail : function(){
-            for(let i=0; i<10; i++){
+            for(let i=0; i<this.numeroEmail; i++){
                 axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
                 .then(response => {
                     if(response.data.success == true){
                         this.email.push(response.data.response);
+                    }
+                    if(i == (this.numeroEmail-1)){
+                        this.finish = true;
                     }
                 });
             }
